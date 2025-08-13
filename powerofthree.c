5bd963777d6523f62e91777a6f3c56ac9072d05e
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-08-13 22:08:42 +0300

    add Power of Three Problem Solution

---
+++ powerofthree.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPowerOfThree(int n);
+
+int main() {
+    printf("%d", isPowerOfThree(243));
+    return 0;
+}
+
+bool isPowerOfThree(int n) {
+    if (n == 0) {
+        return false;
+    } else {
+        double val = log10(n) / log10(3);
+        if (val == floor(val)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
