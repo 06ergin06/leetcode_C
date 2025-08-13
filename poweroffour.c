5f94f234bddde500757d445be67ee861b251cf53
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-08-14 00:31:18 +0300

    added poweroftwo and poweroffour problems

---
+++ poweroffour.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPowerOfFour(int n);
+
+int main() {
+    printf("%d", isPowerOfFour(4));
+    return 0;
+}
+
+bool isPowerOfFour(int n) {
+    if (n == 0) {
+        return false;
+    } else {
+        double val = log10(n) / log10(4);
+        if (val == floor(val)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
---
+++ poweroftwo.c
@@ -0,0 +1,23 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+bool isPowerOfTwo(int n);
+
+int main() {
+    printf("%d", isPowerOfTwo(4));
+    return 0;
+}
+
+bool isPowerOfTwo(int n) {
+    if (n == 0) {
+        return false;
+    } else {
+        double val = log10(n) / log10(2);
+        if (val == floor(val)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
