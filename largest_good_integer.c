24482585385bf56534af8636760fa1a7ecfe8269
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-08-14 10:30:59 +0300

    add largestGoodInteger problem

---
+++ largest_good_integer.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+char *largestGoodInteger(char *num);
+
+int main() {
+    printf("%s", largestGoodInteger("4981904820931099993021308888"));
+    return 0;
+}
+
+char *largestGoodInteger(char *num) {
+    static char sbs[10][4] = {"999", "888", "777", "666", "555", "444", "333", "222", "111", "000"};
+    int i = 0;
+    char *dest;
+    while (i < 10) {
+        dest = strstr(num, sbs[i]);
+        if (dest != NULL) {
+            return sbs[i];
+        }
+        i++;
+    }
+    return "";
+}
