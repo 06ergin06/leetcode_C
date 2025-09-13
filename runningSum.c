b01037dac600fd086acef8ebc5997f58028efdd2
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-09-13 16:29:58 +0300

    add runningSum

---
+++ runningSum.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int *runningSum(int *nums, int numsSize, int *returnSize) {
+    for (int i = 1; i < numsSize; i++) {
+        nums[i] += nums[i - 1];
+    }
+    *returnSize = numsSize;
+    return nums;
+}
