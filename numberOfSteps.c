391a69ca36bdb75e64895309ddf3fd2774b02312
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-09-13 16:58:16 +0300

    add number of steps

---
+++ numberOfSteps.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int numberOfSteps(int num) {
+    int r = 0;
+    while (num > 0) {
+        if (num % 2 == 0) {
+            num /= 2;
+        } else {
+            num--;
+        }
+        r++;
+    }
+    return r;
+}
