c313fd9b4b67a614f528487351d385fe00b1d3fc
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-09-21 01:23:27 +0300

    add happy number

---
+++ happyNumber.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <stdbool.h>
+#include <stdio.h>
+
+int sumOfSquares(int n) {
+    int res = 0;
+    while (n > 0) {
+        res += pow(n % 10, 2);
+        n /= 10;
+    }
+    return res;
+}
+
+bool isHappy(int n) {
+    int slow = sumOfSquares(n);
+    int fast = sumOfSquares(sumOfSquares(n));
+    while (slow != fast) {
+        slow = sumOfSquares(slow);
+        fast = sumOfSquares(sumOfSquares(fast));
+    }
+    return fast == 1;
+}
