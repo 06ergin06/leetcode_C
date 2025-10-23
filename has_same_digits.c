9a0aca87184bdcc3e144e5e6afdfbfd5cbb84c76
İbrahim Hakkı Ergin <ibrahimh.ergin@gmail.com>
2025-10-23 22:03:48 +0300

    Check If Digits Are Equal in String After Operations I

---
+++ has_same_digits.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <string.h>
+
+bool	hasSameDigits(char *s)
+{
+	int	i;
+	int	len;
+
+	i = 0;
+	len = strlen(s);
+	while (s[i] && s[i + 1] && len != 2)
+	{
+		s[i] = (((s[i] - '0') + (s[i + 1] - '0')) % 10) + '0';
+		i++;
+		if (i == len - 1 && i != 1)
+		{
+			i = 0;
+			len--;
+		}
+	}
+	return (s[0] == s[1]);
+}
+
+int	main(void)
+{
+	hasSameDigits("5913");
+}
